eb834a1aee128d857abc742efa289be9f876cd1c
Jo <fkelley318@gmail.com>
2025-05-07 00:24:05 -0500

    Add replay functionality
    - Removes user's ability to interact until 'Play again?' button is clicked
    - Resets states

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let result = document.getElementById("result");
+let user = document.getElementById("userGuess");
+let com = document.getElementById("compGuess");
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let list = ['rock', 'paper', 'scissors'];
+let rply = document.getElementById("replay");
+
 function shoot(hand) {
   var comp = Math.floor(Math.random() * 2);
   if(comp == 0) {
@@ -8,20 +17,20 @@ function shoot(hand) {
     comp = "scissors";
   }
 
-  let result = document.getElementById('result');
-  let user = document.getElementById('userGuess');
   user.innerText = hand;
-  let com = document.getElementById('compGuess');
   com.innerText = comp;
 
   let uGuess = document.getElementById(hand);
   let cGuess = document.getElementById(comp);
-  uGuess.style.backgroundColor = "lightgreen";
-  cGuess.style.backgroundColor = "yellow";
+  if(hand!=comp) {
+    uGuess.classList.add("userGuess");
+    cGuess.classList.add("compGuess");
+  }
 
   if(hand == comp) {
-    result.innerText = "User ties with Computer. No win.";
-    uGuess.style.backgroundColor = "red";
+    result.innerText = "User ties with Computer. Nobody wins.";
+    uGuess.classList.add("tie");
+    cGuess.classList.add("tie");
   } else if(hand == 'rock') {
     if(comp == 'scissors') {
       result.innerText = "User beats Computer with rock.";
@@ -39,6 +48,41 @@ function shoot(hand) {
       result.innerText = "User beats Computer with scissors.";
     } else {
       result.innerText = "Computer beats User with rock."
+    }
+  }
+
+  rply.style.display = "block";
+
+  for (i=0; i<list.length; i++) {
+    let lItem = document.getElementById(list[i]);
+    lItem.removeAttribute("onclick");
+    lItem.style.cursor = "not-allowed";
+    lItem.classList.add("done");
+    if(hand!=list[i] && comp!=list[i]) {
+      lItem.classList.add("played");
+    }
+  }
+}
+
+function replay() {
+  rply.style.display = "none";
+
+  for (i=0; i<list.length; i++) {
+    let lItem = document.getElementById(list[i]);
+    lItem.onclick = shoot;
+    /*nobody else would give this straight answer for some reason...?
+    https://stackoverflow.com/a/23412811*/
+    lItem.setAttribute("onclick", "shoot('" + list[i] + "')");
+    lItem.style.cursor = "pointer";
+    lItem.classList.remove("done");
+    if(list[i]!=user.innerText && list[i]!=com.innerText) {
+      lItem.classList.remove("played");
+    } else if (user.innerText ===  com.innerText) {
+      lItem.classList.remove("tie");
+    } else if (list[i] === user.innerText) {
+      lItem.classList.remove("userGuess");
+    } else {
+      lItem.classList.remove("compGuess");
     }
   }
 }
